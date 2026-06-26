5f2787c5f0274e24094e11f3cf2e5c6cd63ebc57
cffjff758-wq <cffjff758@gmail.com>
2026-06-26 03:31:26 +0300

    Refactor kernel_main to use constants and new message
    Updated video memory initialization and changed the message displayed on the screen.

--- kernel.c
+++ kernel.c
@@ -1,22 +1,24 @@
+#define VIDEO_ADDRESS 0xB8000
+#define WHITE_ON_BLACK 0x0F
+
 void kernel_main() {
-    // Address of VGA text mode video memory in 64-bit
-    char *video_memory = (char *) 0xB8000;
-    
-    // The message to print on the screen
-    const char *message = "Hello from 64-bit iPad OS!";
-    
+    char *video_memory = (char*) VIDEO_ADDRESS;
+
+    for (int i = 0; i < 80 * 25 * 2; i += 2) {
+        video_memory[i] = ' ';
+        video_memory[i+1] = WHITE_ON_BLACK;
+    }
+
+    const char *str = "Welcome to your 64-Bit Operating System!";
     int i = 0;
-    // Loop through each character of the string
-    while (message[i] != '\0') {
-        // Set the character byte
-        video_memory[i * 2] = message[i];
-        // Set the attribute byte (0x02 = Green text on Black background)
-        video_memory[i * 2 + 1] = 0x02;
+
+    while (str[i] != '\0') {
+        video_memory[i * 2] = str[i];
+        video_memory[i * 2 + 1] = WHITE_ON_BLACK;
         i++;
     }
 
-    // Infinite loop to halt the CPU execution safely
-    while(1) {
+    while (1) {
         __asm__ __volatile__("hlt");
     }
 }
